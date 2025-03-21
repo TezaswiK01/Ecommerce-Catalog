2ea5f5b7642dfa6aa217cb53e4e63381d0dbf043
unknown <tezaswikoppuravuri01@gmail.com>
2025-03-21 11:51:54 +0530

    after adding category filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import ProductList from "./ProductList";
+import CategoryList from "./CategoryList";
 import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
 
@@ -16,7 +17,7 @@ function App() {
       .then((response) => response.json())
       .then((data) => setProducts(data));
 
-    fetch("http://localhost:8080/api/product")
+    fetch("http://localhost:8080/api/categories")
       .then((response) => response.json())
       .then((data) => setCategories(data));
   }, []);
@@ -26,13 +27,23 @@ function App() {
   const handleSortChange = (event) => {
     setSortOrder(event.target.value);
   };
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId ? Number(categoryId) : null);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    return selectedCategory ? product.category.id === selectedCategory : true;
+  });
   return (
     <div className="container">
       <h1 className="my-4">Product Catalog</h1>
       <div className="row align-items-center mb-4">
         <div className="col-md-3 col-sm-12 mb-12">
           {/* //display cateogry filter */}
-          <p>Category filter</p>
+          <CategoryList
+            categories={categories}
+            onSelect={handleCategorySelect}
+          />
         </div>
         <div className="col-md-5 col-sm-12 mb-2">
           <input
@@ -44,13 +55,13 @@ function App() {
         </div>
         <div className="col-md-4 col-sm-12 mb-2">
           <select className="form-control" onChange={handleSortChange}>
-            <option value="desc">Sort by price : High c to Low</option>
+            <option value="desc">Sort by price : High to Low</option>
             <option value="asc">Sort by price: Low to high</option>
           </select>
         </div>
       </div>
-      {products.length ? (
-        <ProductList products={products} />
+      {filteredProducts.length ? (
+        <ProductList products={filteredProducts} />
       ) : (
         <p>Products are not there</p>
       )}
